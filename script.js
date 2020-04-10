50e07dca7a75a5b9947760fbc1e6bcc30eca09f6
Watts <swatts204@cable.comcast.com>
2020-04-10 17:00:38 -0400

    reallyImDone

--- script.js
+++ script.js
@@ -1,7 +1,2 @@
-var person = alert("Thanks to Marcos Cabrera for the awesome art on this page!");
+ alert("Thanks to Marcos Cabrera for the awesome art on this page!");
 
-// if (person == null || person == "") {
-//   txt = "User cancelled the prompt.";
-// } else {
-//   txt = "Hello " + person + "! How are you today?";
-// }
